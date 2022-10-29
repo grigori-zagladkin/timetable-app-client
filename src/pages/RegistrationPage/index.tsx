cbbe830bb54faaab9b6f275d1dfd91fc0e9d8ffb
grigori-zagladkin <grigorizagladkin@gmail.com>
2022-10-30 01:06:54 +0300

    2 commit

---
+++ src/pages/RegistrationPage/index.tsx
@@ -0,0 +1,11 @@
+import { FC, Fragment } from "react";
+
+const RegistrationPage: FC = () => {
+    return (
+        <Fragment>
+            <div></div>
+        </Fragment>
+    );
+};
+
+export default RegistrationPage;
